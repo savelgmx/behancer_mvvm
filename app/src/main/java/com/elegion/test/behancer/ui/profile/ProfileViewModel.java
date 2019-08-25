f8ad52455c46bd3b006d0c3c66e137c495c6c040
savelgmx <savel@gmx.com>
2019-08-25 13:02:14 +0700

    #2 task-1 ProfileViewModel created and named as necessary local branch just created

--- app/src/main/java/com/elegion/test/behancer/ui/profile/ProfileViewModel.java
+++ app/src/main/java/com/elegion/test/behancer/ui/profile/ProfileViewModel.java
@@ -2,5 +2,5 @@ package com.elegion.test.behancer.ui.profile;
 
 import android.arch.lifecycle.ViewModel;
 
-public class ProfileVieModel extends ViewModel {
+public class ProfileViewModel extends ViewModel {
 }
