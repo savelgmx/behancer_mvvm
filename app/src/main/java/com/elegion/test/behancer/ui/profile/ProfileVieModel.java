a4bda38d24d21fe4d0b18ab4203e7589e8a19756
savelgmx <savel@gmx.com>
2019-08-25 13:00:10 +0700

    #2 task-1 ProfileViewModel created local branch just created

---
+++ app/src/main/java/com/elegion/test/behancer/ui/profile/ProfileVieModel.java
@@ -0,0 +1,6 @@
+package com.elegion.test.behancer.ui.profile;
+
+import android.arch.lifecycle.ViewModel;
+
+public class ProfileVieModel extends ViewModel {
+}
--- app/src/main/java/com/elegion/test/behancer/ui/projects/ProjectsActivity.java
+++ app/src/main/java/com/elegion/test/behancer/ui/projects/ProjectsActivity.java
@@ -10,6 +10,17 @@ import com.elegion.test.behancer.data.Storage;
  * Created by Vladislav Falzan.
  */
 
+/*
+Добавить реализацию ViewModel на экран Profile с помощью databinding library
+
+Критерии проверки
+1) ProfileActivity наследуется от SingleFragmentActivity,
+в R.layout.fr_profile есть SwipeRefreshLayout
+(по аналогии с рефакторингом экрана проектов, см. уроки)
+2) В fr_profile добавлены необходимые изменения для работы с databinding library
+3) Создан класс ProfileViewModel, который поставляет данные для ProfileFragment/fr_profile
+4) Логика загрузки, отображения, обработки ошибок перенесена в ProfileViewModel/fr_pofile
+ */
 public class ProjectsActivity extends SingleFragmentActivity implements Storage.StorageOwner {
 
     @Override
