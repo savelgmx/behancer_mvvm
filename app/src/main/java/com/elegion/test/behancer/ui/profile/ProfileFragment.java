944b69efa89871ec80624daa3af53c5871e96815
savelgmx <savel@gmx.com>
2019-08-28 16:31:11 +0700

    #6 ProfileFragment изменен TODO databinding in  Layout

--- app/src/main/java/com/elegion/test/behancer/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/elegion/test/behancer/ui/profile/ProfileFragment.java
@@ -1,5 +1,8 @@
 package com.elegion.test.behancer.ui.profile;
 
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -26,18 +29,8 @@ import com.squareup.picasso.Picasso;
 public class ProfileFragment extends Fragment {
 
     public static final String PROFILE_KEY = "PROFILE_KEY";
-
-    private RefreshOwner mRefreshOwner;
-    private View mErrorView;
-    private View mProfileView;
     private String mUsername;
-
     private ProfileViewModel mProfileViewModel;
-
-    private ImageView mProfileImage;
-    private TextView mProfileName;
-    private TextView mProfileCreatedOn;
-    private TextView mProfileLocation;
 
     public static ProfileFragment newInstance(Bundle args) {
         ProfileFragment fragment = new ProfileFragment();
@@ -50,12 +43,27 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+        mUsername=getArguments().getString(PROFILE_KEY);
+
+
         if (context instanceof Storage.StorageOwner) {
             Storage storage = ((Storage.StorageOwner) context).obtainStorage();
-            mProfileViewModel = new ProfileViewModel(storage,mUsername);
+            //   mProfileViewModel = new ProfileViewModel(storage,mUsername);
+
+            ViewModelProvider.Factory factory = new ViewModelProvider.Factory() {
+                @NonNull
+                @Override
+                public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+                    return (T) new ProfileViewModel(storage,mUsername);
+                }
+            };
+            mProfileViewModel = ViewModelProviders.of(this, factory).get(ProfileViewModel.class);
+
+
         }
 
     }
+
 
     @Nullable
     @Override
@@ -66,59 +74,4 @@ public class ProfileFragment extends Fragment {
 
     }
 
- /*   @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        mErrorView = view.findViewById(R.id.errorView);
-        mProfileView = view.findViewById(R.id.view_profile);
-
-        mProfileImage = view.findViewById(R.id.iv_profile);
-        mProfileName = view.findViewById(R.id.tv_display_name_details);
-        mProfileCreatedOn = view.findViewById(R.id.tv_created_on_details);
-        mProfileLocation = view.findViewById(R.id.tv_location_details);
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-        if (getArguments() != null) {
-            mUsername = getArguments().getString(PROFILE_KEY);
-        }
-
-        if (getActivity() != null) {
-            getActivity().setTitle(mUsername);
-        }
-
-        mProfileView.setVisibility(View.VISIBLE);
-
-        onRefreshData();
-    }
-
-    @Override
-    public void onRefreshData() {
-        ProfileViewModel.getProfile();
-    }
-
-    private void bind(User user) {
-        Picasso.with(getContext())
-                .load(user.getImage().getPhotoUrl())
-                .fit()
-                .into(mProfileImage);
-        mProfileName.setText(user.getDisplayName());
-        mProfileCreatedOn.setText(DateUtils.format(user.getCreatedOn()));
-        mProfileLocation.setText(user.getLocation());
-    }
-
-    @Override
-    public void onDetach() {
-*//*
-        mStorage = null;
-        mRefreshOwner = null;
-        if (mDisposable != null) {
-            mDisposable.dispose();
-        }
-*//*
-        super.onDetach();
-    }
-*/
 }
