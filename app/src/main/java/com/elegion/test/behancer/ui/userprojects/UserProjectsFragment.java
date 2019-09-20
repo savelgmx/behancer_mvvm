ed763b88f3b8ac18cc7659863d8bb6668b2353b1
savelgmx <savel@gmx.com>
2019-09-20 08:30:48 +0700

    #9 Измениения в UserProjectsFragment класс их надо потестить

--- app/src/main/java/com/elegion/test/behancer/ui/userprojects/UserProjectsFragment.java
+++ app/src/main/java/com/elegion/test/behancer/ui/userprojects/UserProjectsFragment.java
@@ -1,6 +1,25 @@
 package com.elegion.test.behancer.ui.userprojects;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
+import com.elegion.test.behancer.ui.profile.ProfileActivity;
+import com.elegion.test.behancer.ui.profile.ProfileFragment;
+import com.elegion.test.behancer.ui.profile.ProfileViewModel;
+
 public class UserProjectsFragment extends Fragment {
+    private UserProjectsViewModel mUserProjectsViewModel;
+    private ProfileViewModel.OnItemClickListener mOnItemClickListener = username->{
+        //здесь должен быть вызов списка проектов пользвателя
+
+      Intent intent = new Intent(getActivity(), UserProjectsActivity.class);
+      Bundle args = new Bundle();
+      args.putString(ProfileFragment.PROFILE_KEY, username);
+      intent.putExtra(ProfileActivity.USERNAME_KEY, args);
+      startActivity(intent);
+
+
+    };
+
 }
